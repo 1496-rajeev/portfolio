35470fbdd42d1cdc6f38f75ae85abe09f54dd0c8
Rajeev Verma <rajeevverma@Pritam-MacBook-Pro.local>
2022-08-21 09:55:30 +0530

    medium added

--- components/home/hero-section.js
+++ components/home/hero-section.js
@@ -4,12 +4,14 @@ import React from 'react'
 //style
 import { heading, subHeading } from '../../assets/typography'
 import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin, AiFillInstagram } from "react-icons/ai";
+import { BsMedium } from "react-icons/bs";
 
 export default function HeroSection() {
   const socialLinks = [
     { name: "Github", icon: <AiFillGithub size={30} />, link: "https://github.com/1496-rajeev" },
     { name: "LinkedIn", icon: <AiFillLinkedin size={30} />, link: "https://www.linkedin.com/in/1496-rajeev/" },
     { name: "Twitter", icon: <AiFillTwitterCircle size={30} />, link: "https://twitter.com/RajeevVerma1496" },
+    { name: "Medium", icon: <BsMedium size={30} />, link: "https://medium.com/@1496rajeev" },
     { name: "Instagram", icon: <AiFillInstagram size={30} />, link: "https://www.instagram.com/1496rajeev.verma/" }
   ]
   return (
