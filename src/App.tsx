9e4d02fba507dd22b9b510bba8a9faef31bdc88f
Rajeev verma <rajeevverma@Rajeevs-MacBook-Air.local>
2024-07-07 23:42:15 +0530

    coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import React from "react";
 function App() {
-  return <>wait...</>;
+  return (
+    <div
+      style={{
+        margin: "auto",
+        border: "3px solid green",
+        padding: "10px",
+        fontWeight: "bold",
+        fontSize: "30px",
+      }}
+    >
+      Coming soon 😊
+    </div>
+  );
 }
 
 export default App;
